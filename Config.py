1a3050764bd4b6d9acd92d6faf5cbe13c562fa89
Kaveesha Induwara <72741520+kaviya-admin@users.noreply.github.com>
2020-11-26 17:47:32 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,19 +3,18 @@ import os
 class Config():
   ENV = bool(os.environ.get('ENV', False))
   if ENV:
-    BOT_TOKEN = os.environ.get("1452280871:AAGN1MxkpW6a5FeOp7nQwllCeoK4Y-ajf5Q", None)
-    DATABASE_URL = os.environ.get("postgres://kgbbpqxosfdhna:40883e4357b01f7dbb4e743d4220ae6affbb8844cf40a347fae7ada0a8699404@ec2-34-202-65-210.compute-1.amazonaws.com:5432/dfsgfca3skst64
-", None)
-    APP_ID = os.environ.get("1994857", 6)
-    API_HASH = os.environ.get("fce76b799d09790680bb71ee997ff38f", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    DATABASE_URL = os.environ.get("DATABASE_URL", None)
+    APP_ID = os.environ.get("APP_ID", 6)
+    API_HASH = os.environ.get("API_HASH", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1452280871:AAGN1MxkpW6a5FeOp7nQwllCeoK4Y-ajf5Q"
+    DATABASE_URL = "postgres://kgbbpqxosfdhna:40883e4357b01f7dbb4e743d4220ae6affbb8844cf40a347fae7ada0a8699404@ec2-34-202-65-210.compute-1.amazonaws.com:5432/dfsgfca3skst64"
+    APP_ID = "1994857"
+    API_HASH = "fce76b799d09790680bb71ee997ff38f"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
@@ -25,13 +24,13 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        "Channel Manager**\n__Force group members to join linked channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
         
         "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
         
         "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
         
-        "**Developed by @Kaveesha_Induwara**"
+        "**Developed by @Kaveesha_Induwara"
       ]
 
       START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
